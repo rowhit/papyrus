5f1a1224d2117316b7660995e284cac7828da558
sgerard <sgerard>
2010-04-16 13:10:18 +0000

    Add download info

--- download.php
+++ download.php
@@ -10,17 +10,13 @@
  *    
  *******************************************************************************/
 
-	$pageTitle 		= "MyProject - Download";
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
+	$pageTitle 		= "Papyrus - Download";
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/mdt/papyrus/style.css"/>');
 
 	$html  = <<<EOHTML
 <div id="midcolumn">
 <h2>$pageTitle</h2>
 <p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
-
-<h3>Galileo - Eclipse 3.5</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p>
 
 <h3>Helios - Eclipse 3.6 (unreleased)</h3>
 <p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
