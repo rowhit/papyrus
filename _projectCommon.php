3a51571387de143e65b51b22f5ecd3821f7c12d9
sgerard <sgerard>
2010-04-16 12:39:40 +0000

    Update of the main background banner to integrate the Papyrus Logo.

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,7 +8,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Sebastien Gerard
+ * Sebastien Gerard, CEA LIST
  *    
  *******************************************************************************/
 
