1a8b40a6ce6373cbd7ffbb134fcaf28b1da8c106
sgerard <sgerard>
2010-05-10 10:32:05 +0000

    Commit update of Papyrus home page.

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	
 	
 	// 	# Paste your HTML content between the EOHTML markers!
-	$html = file_get_contents('pages/PapyrusHomePage.html');
+	$html = file_get_contents('homePage/PapyrusHomePage.html');
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
