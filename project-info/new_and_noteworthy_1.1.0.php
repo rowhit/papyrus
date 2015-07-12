07b7f0b3af5ee82cd56e7441c74accfb5b9d0f15
Remi Schnekenburger <remi.schnekenburger@cea.fr>
2015-07-12 03:19:54 +0200

    Updatenew and noteworthy for 1.1.0 version
    Change-Id: I975bc5acb446e59fd7a612cc9c25ebdc3175e9e5

--- project-info/new_and_noteworthy_1.1.0.php
+++ project-info/new_and_noteworthy_1.1.0.php
@@ -24,33 +24,118 @@ ob_start();
 <!-- Middle part -->
 <div id="midcolumn">
 
-<h1>What's new in Papyrus 1.1.0?</h1>
+<h1>New and Noteworthy - Papyrus 1.1.0 (Mars)</h1>
 
-<h2>General</h2>
+<h2>What's new?</h2>
 
+<h3>Diagram synchronization support</h3>
+<p align="JUSTIFY">
+You can now activate synchronization on your diagram or part of your diagram between the graphical representation and the semantic model: What you see is what you have in your model!
+</p>
+<a href="http://youtu.be/uo2jpPd-n2s">See a sample video on this feature</a>
 
-
-<h2>Diagrams</h2>
-<h3>Figure customization and shapes in all diagrams</h3>
-<p>Papyrus diagrams now relies mainly on one highly customizable figure, which can be parameterized though CSS or advanced appearance property view. All diagrams support also the shapes framework, allowing you to define your own representation for your language!</p>
-<div id="screenshot_shapes">
-	<img width="640" src="../images/shapes.png"/>profile
-</div>
-
-<h3>Element Types Configuration</h3>
-<p>You can now define in a central place all the actions on your model, e.g. the creation, the deletion and the edition of the elements. You can reuse these action definitions in the model explorer, the palettes, the modeling assistants, etc.</p>
 
 <h3>Modeling assistants dedicated to your language</h3>
-<p>The modeling assistants are popup shortcuts that help you create new elements in the diagram. You can now create in Papyrus a model of your diagram assistants for your own profile, and load them into your running Papyrus instance for a live test./p>
+<p align="JUSTIFY">
+The modeling assistants are popup shortcuts that help you create new elements in the diagram. You can now create in Papyrus a model of your diagram assistants for your own profile, and load them into your running Papyrus instance for a live test.
+</p>
 <div id="screenshot_modelingassistants">
-	<img width="640" src="../images/screenshot_modelingassistants.png"/>profile
+	<img width="524" src="../images/screenshot_modelingassistants.png"/>profile
 </div>
 
-<h2>Tests and Debug</h2>
+<h3>Hierarchical tables</h3>
+<p align="JUSTIFY">
+You can now define and use hierarchical tables in Papyrus. Hierarchical tables let you see any custom structure corresponding to UML2 metamodel or your own structure
+</p>
+<div id="screenshot_hierarchicaltables">
+	<img width="640" src="../images/hierarchicaltables.png"/>
+</div>
+
+<h2>What has been updated since version 1.0?</h2>
+<h3>Figure customization and custom shape support in all diagrams</h3>
+<p align="JUSTIFY">
+Papyrus diagrams now relies on one highly customizable figure, which can be parameterized though CSS or extra advanced appearance property view. All diagrams support also the shapes framework, allowing you to define your own representation for your language in any UMLs standard diagram, like activity diagram!
+</p>
+<div id="screenshot_shapes">
+	<img width="296" src="../images/shapes.png"/>
+</div>
+
+<h3>Activity Diagram</h3>
+<p align="JUSTIFY">
+Activity diagram has been updated in Mars version to get better user experience. Code has been modernized to support all new Papyrus features, including better stereotype support and shape customization. An example of this customization will be available soon, with the capability of process modeling based on the BPMN profile and Activity diagram customization.
+</p>
+<div id="screenshot_bpmn">
+	<img width="515" src="../images/BPMN_preview.png"/>
+</div>
+
+<h3>Stereotype Support</h3>
+<p align="JUSTIFY">
+Stereotype display has been upgraded in Papyrus Mars to support customization using stylesheets. The stereotypes applications can be also now displayed by default when displaying elements. For example, stereotyped elements dropped from model explorer can now display directly their stereotype, without any additional user action. Finally, the user interface to select stereotype display has been simplified.
+</p> 
+<div id="screenshot_stereotypestable">
+	<img width="515" src="../images/StereotypeDisplay.png"/>
+</div>
+
+<h3>Model explorer creation menu</h3>
+<p align="JUSTIFY">
+you can now create relationships like Associations and Dependencies using the contextual menu on right click from the model explorer. The first element selected will be the source of your link, and a selection dialog let you pick up the target of your link. 
+</p> 
+<div id="screenshot_modelexplorerrelationship">
+	<img width="320" src="../images/ModelExplorerCreateRelationship.png"/>
+</div>
+
+<h3>Papyrus EMF Compare integration</h3>
+<p align="JUSTIFY">
+The support for collaborative modeling in Papyrus relying on EMF Compare component has been improved. You can see more on that page:  
+</p>
+
+<h3>Papyrus Search</h3>
+<p align="JUSTIFY">
+You can now search for a given set of stereotypes or values of properties of stereotype from Papyrus search UI
+</p>
+<div id="screenshot_searchimproved">
+	<img width="535" src="../images/SearchImproved.png"/>
+</div>
+
+<h3>Papyrus Wizard</h3>
+<p align="JUSTIFY">
+Model and project creation wizards have been updated to give you more opportunities. You can for example select a name for the project, the model file and the root element of your model. You can also apply at the same time a profile and use a predefined template for your new model.
+</p>
+<div id="screenshot_wizardimproved">
+	<img width="356" src="../images/WizardImproved.png"/>
+</div>
+
+<h3>Advanced multiplicity editor</h3>
+<p align="JUSTIFY">
+The property view for multiplicity elements provides now an advanced widget. This optional widget lets you enter a complex multiplicity like [0..MAX]. You can switch between the simple and the advanced one directly from the property view or from the preferences.
+</p>
+<div id="screenshot_multiplicity">
+	<img width="635" src="../images/AdvancedMultiplicity.png"/>
+</div>
+
+<h2>Under the hood</h2>
+<h3>Element Types Configuration</h3>
+<p align="JUSTIFY">
+You can now define in a central place all the actions on your model, e.g. the creation, the deletion and the edition of the elements. You can reuse these action definitions in the model explorer, the palettes, the modeling assistants, etc.
+</p>
+
+<h3>Diagram Expansion</h3>
+<p align="JUSTIFY">
+The existing diagram definitions can now be extended using Diagram Expansion framework. This framework let you add any kind of representation to existing ones, like new custom compartments for classifiers representations. This new feature is currently used for the incoming support of SysML1.4 in Papyrus, and also for the incoming Papyrus-RT project.
+</p> 
+
+<h3>Test and debug</h3>
 <p align="JUSTIFY">
 More than <a href="https://bugs.eclipse.org/bugs/buglist.cgi?action=wrap&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&chfield=resolution&chfieldfrom=2015-01-01&chfieldto=Now&classification=Modeling&component=Core&component=Diagram&component=Others&component=Table&component=Views&product=Papyrus&version=1.1.0">400 bugs</a> have been closed for version 1.1.0 since January 2014, and more than <a href="https://hudson.eclipse.org/papyrus/job/Papyrus-Master-Tests/">10.000</a> unit tests are run against Papyrus every nights.  
 </p>
 </div>
+
+<h3>Quality improvments</h3>
+<p align="JUSTIFY">
+Thanks to new frameworks and better code generation, Papyrus Mars has more features than Luna version while using less code. 20% of lines of code has been removed, being centralized in common places to reduce maintenance costs and improve user experience.  
+</p>
+</div>
+
 
 <?php
 
